0d99e7a90bd44cd8b5ca4fe993fe477c37645963
mohamed06H <114097516+mohamed06H@users.noreply.github.com>
2024-11-03 14:18:49 +0100

    Update 28-find-index-of-first-occurrence-string.py

--- Code/array-string/28-find-index-of-first-occurrence-string.py
+++ Code/array-string/28-find-index-of-first-occurrence-string.py
@@ -1,3 +1,4 @@
+# Easy solution
 class Solution(object):
     def strStr(self, haystack, needle):
         """
@@ -6,3 +7,16 @@ class Solution(object):
         :rtype: int
         """
         return haystack.find(needle)
+# Second solution         
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        for i in range(0, len(haystack)-len(needle)):
+            # Compare each substring of length: len(needle) to needle
+            if haystack[i:len(needle)] == needle:
+                return i
+        return -1
